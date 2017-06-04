f70146f98654538eef6f8e433e0a9762240a5c79
John-George <johngeorgesample@gmail.com>
2017-06-03 22:19:03 -0400

    Revert "Revert "Started changing variable names""
    This reverts commit c8922673b08ef2ef9f808540a8e2637f615b2699.

--- metaInfo.php
+++ metaInfo.php
@@ -6,27 +6,30 @@
   $html = new simple_html_dom();
   $html->load_file($_POST["myUrl"]); 
 
-  //website name
-  $website_title = $html->find("meta[property='og:site_name']", 0)->content;
+  //website title
+  $property-ogSite_name = $html->find("meta[property='og:site_name']", 0)->content;
 
-  //page title
+  //article title
   $meta_title = $html->find('title',0)->innertext;
-  $og_title = $html->find("meta[property='og:title']", 0)->content;
+  $property-ogTitle = $html->find("meta[property='og:title']", 0)->content;
 
   //publisher
-  $publisher = $website_title;
+  $publisher = $property-ogSite_name;
   
   //Dates
   $accessed_date = date('d M Y');
-  $published_date = $html->find("meta[property='article:published_time']", 0)->content;
-  $display_date = $html->find("meta[name='DisplayDate']", 0)->content;
-  $time_date = $html->find('time',0)->innertext; //NPR uses this - ugh
+
+  $property-pubishedTime = $html->find("meta[property='article:published_time']", 0)->content;
+  $property-ogPubishedTime = $html->find("meta[property='og:article:published_time']", 0)->content;
+  $name-DisplayDate = $html->find("meta[name='DisplayDate']", 0)->content;
+
+  $time_date = $html->find('time',0)->innertext;
   
   //author - fix for multi-names
-  $meta_author = $html->find("meta[name='author']", 0)->content;
+  $name-author = $html->find("meta[name='author']", 0)->content;
   //$meta_Author = $html->find("meta[name='Author']", 0)->content; //WIRED is a prime example of this. Capitalizing letter is a totally new tag
-  $meta_property_author = $html->find("meta[property='author']", 0)->content;
-  $meta_article_author = $html->find("meta[property='article:author']", 0)->content;
+  $property-author = $html->find("meta[property='author']", 0)->content;
+  $property-articleAuthor = $html->find("meta[property='article:author']", 0)->content;
 ?>
 
 <?php //convert article:published_time to d M Y
@@ -76,23 +79,12 @@
 ?>
 
 <p><b>url</b>: <?php echo $_POST["myUrl"] ?></p>
-<p><b>website title</b>: <?php echo $website_title?></p> <!--if null, use article title-->
+<p><b>website title</b>: <?php echo $property-ogSite_name?></p> <!--if null, use article title-->
 <p><b>article title</b>: <?php echo $og_title?></p>
 <p><b>publisher</b>: <?php echo $publisher?></p> <!-- might be same as website_title -->
 <p><b>electronically published</b>: <?php echo $full_publish_date?></p>
 <p><b>Date Accessed</b>: <?php echo $accessed_date?> </p>
 
-<?php 
-  if($meta_author == null && $meta_property_author == null) {
-    echo "<p>no author</p>"; //ask user to input missing info
-  }
-  elseif($meta_author == null && meta_property_author != null) {
-    echo "<p><b>author</b>: " . $meta_property_author . "</p>";
-  }
-  elseif ($meta_property_author == null && meta_author != null){
-    echo "<p><b>author</b>: " . $meta_author . "</p>";
-  }
-?>
 
 
 <?php //author styling. lastName, firstName
@@ -107,7 +99,7 @@
 <p><?php echo $author_last_name . ", " . $author_first_name . ". \"" . $og_title . "\". " . "<i>" . $publisher . "</i>. " . $website_title . ", " . $full_publish_date . ". " . "Web." . " " . $accessed_date ?></p>
 
 
-<a href="http://localhost:8888/app">click to go back</a>
+<a href="http://localhost:8888/">click to go back</a>
 
 </body>
 </html>
