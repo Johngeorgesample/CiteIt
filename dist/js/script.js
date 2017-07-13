a8fe3a4a1d4b54ec7551f1288ac435ec892fce11
John-George <johngeorgesample@gmail.com>
2017-07-12 22:23:53 -0400

    started to move js for localstorage to script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,20 @@
-function hello() {
-	console.log("helo");
-}
+  // var output = ''; 
+  // localStorage.setItem(Math.random(), document.getElementById('finalCitationBox').innerHTML);
+
+  // for (var key in localStorage) {
+  //   if(localStorage[key] != document.getElementById('finalCitationBox').innerHTML) {
+  //     console.log(key + ':' + localStorage[key]); //for debugging
+  //   }
+  // }
+
+  // for (var key in localStorage) {
+  // 	if(localStorage[key] != document.getElementById('finalCitationBox').innerHTML) {
+  //     output = output+(localStorage[key])+'<br><br>';
+  //   }
+  // }
+
+  // $('#DivToPrintOut').html(output);
+
+  // function clearS() {
+  // 	localStorage.clear();
+  // }
