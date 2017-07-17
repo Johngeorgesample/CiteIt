f834c7c0461d09ba40476865dd96bce401911c13
John-George <johngeorgesample@gmail.com>
2017-07-17 16:52:19 -0400

    removed !important from fa-trash

--- dist/results.php
+++ dist/results.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>CiteIt!</title>
-  <link rel="stylesheet" href="css/main.css" type="text/css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css"> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="css/main.css" type="text/css">
 
 </head>
 <body>
