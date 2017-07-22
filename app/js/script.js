5ff4e0f6bc9c4008c48f3d95fe189a84d086fa2f
John-George <johngeorgesample@gmail.com>
2017-07-22 14:29:13 -0400

    Moved js to script.js

--- app/js/script.js
+++ app/js/script.js
@@ -9,3 +9,41 @@ function generatePDF() {
   }
   doc.save('myCitations.pdf');
 }
+
+  var output = ''; 
+  localStorage.setItem(document.getElementById('finalCitationBox').innerHTML, document.getElementById('finalCitationBox').innerHTML);
+
+
+  for (var i in localStorage) {
+  	//if(localStorage[i] != document.getElementById('finalCitationBox').innerHTML) {
+      console.log(localStorage[i]); //for debugging
+    //}
+  }
+
+  var j = 0;
+  for (var i in localStorage) { //don't add newly created citation to "older citations"
+  	if(localStorage[i] != document.getElementById('finalCitationBox').innerHTML) {
+      output+= '<p onclick="removeFromlocalStorage(this.id);hideCitation(this.id)" id="'+j+'">' + (localStorage[i])+'</p>';
+      j++;
+    }
+  }
+
+  $('#DivToPrintOut').html(output);
+
+  function clearS() {
+    console.log("localStorage cleared");
+  	localStorage.clear();
+  }
+  
+  function removeFromlocalStorage(citation) {
+    for (var i in localStorage) {
+      if(localStorage[i] === document.getElementById(citation).innerHTML) {
+        console.log("removing " + localStorage[i]);
+        localStorage.removeItem(document.getElementById(citation).innerHTML);
+      }
+    }
+  }
+
+  function hideCitation(citation) {
+    $( "#" + citation ).remove();
+  }
